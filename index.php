23393b2c56b308f5aa7fd2abff46737288ad6337
xwj-scarf <934098774@qq.com>
2018-02-14 12:37:06 +0800

    t

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__) . "/include/include.php");
 $smarty = new Smarty_Oj();
 
-
 $smarty->assign('is_login',1);
 $smarty->assign('name','weijunweijunweijun');
 $smarty->display('home.html');
